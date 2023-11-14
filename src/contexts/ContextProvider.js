d0eb98551b325a1d87a571f9f02f7b422356d444
SantiagoWL <santuagowl@gmail.com>
2023-11-13 23:27:27 -0600

    All code

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,58 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    chart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+
+        localStorage.setItem('themeMode', e.target.value);
+
+        setThemeSettings(false);
+    }
+    
+    const setColor = (color) => {
+         setCurrentColor(color);
+
+        localStorage.setItem('colorMode', color);
+
+        setThemeSettings(false);
+    }
+
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true});
+    }
+    return (
+        <StateContext.Provider
+            value={{
+                //since the key and the value are the same, the ladder can be omitted
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                screenSize, setScreenSize,
+                currentColor, currentMode,
+                themeSettings, setThemeSettings,
+                setMode, setColor
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
